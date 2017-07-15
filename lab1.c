9766da96524cc9564e10466a7dbe0db17c160e88
changchanglccc <lichong06091992@gmail.com>
2017-07-14 22:05:48 -0400

    test

--- lab1.c
+++ lab1.c
@@ -5,24 +5,21 @@
 int main()
 {
 	int nums[10]; //= {3,2,6,5,7,1,8,9,0,4};
-	int i ;
+	int temp;
 	printf("========Bubble Sort===========\n");
 	printf("--Please enter 10 integer numbers --\n");
 
 	for(int i =0; i <10; i++)
 	{
-		printf("-- The number you type in is :\n");
+		printf("-- Please enter the %dth value:\n",(1+i));
 		scanf("%d",&nums[i]);
-		if(i == 10 - 1)
-		{
-			printf("--Typing end ...\n");
-		}
 	}
+	printf("\nTyping End");
 	
 	printf("******Start bubble sort******\n");
 
 	//Bubble sort
-	int temp = 0;
+	
 	for(int i = 0; i < 10; i ++)
 	{
 		for(int j = i + 1; j < 10; j ++)
@@ -35,10 +32,11 @@ int main()
 			}
 		}	
 
+		printf("\nSorted List:\n");
 		//print out for each round
 		for(int x = 0; x <10; x ++)
 		{
-			printf("%d ,",nums[x]);
+			printf("%d ",nums[x]);
 		}
 		printf("\n");
 	}
