762b1e5aa20d399ac20de782284d3a93180f84b2
punittrivedi <punithtrivedi@gmail.com>
2017-07-17 21:30:55 -0400

    Update bubble.pl

--- bubble.pl
+++ bubble.pl
@@ -1,3 +1,8 @@
+/**
+* Bubble Sort With Recursion
+* Example:
+* bubble([10,99,8,2,6,5,4,3,2,1],Sorted).
+*/
 bubble(Input,Sorted) :- swap(Input,List) , ! , bubble(List,Sorted).
                         bubble(Sorted,Sorted).
 swap([A,B|List],[B,A|List]) :- A > B.
