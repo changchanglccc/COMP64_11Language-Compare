e48bd11cd5124bba574ab61aa6417292c52e5b0e
Tann <Tianen.chen@outlook.com>
2017-07-17 21:29:27 -0400

    Update symptom_check_update.pl

--- symptom_check_update.pl
+++ symptom_check_update.pl
@@ -39,8 +39,8 @@ disease([S],D):-symptom(D,S).
 disease([T|H],D):-symptom(D,T),disease(H,D). 
 
 /**
- we input a list of symptoms the patient have, the prolog will output the disases patient may have.
- For example, disease([chills,fever],D). and D will output 'influenza','aids'.
- so more symptoms you input, more precise the disease the system answer you.
-*/
+ * we input a list of symptoms the patient have, the prolog will output the disases patient may have.
+ * For example, disease([chills,fever],D). and D will output 'influenza','aids'.
+ * so more symptoms you input, more precise the disease the system answer you.
+ */
 
