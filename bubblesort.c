07cfce2d30f821c4de4ae847373d03d0cd3d6fc0
Jennifer Chong Li <jennifer06091992@gmail.com>
2017-07-17 23:02:52 -0400

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -20,15 +20,15 @@ int main()
 
 	//Bubble sort
 	
-	for(int i = 0; i < 10; i ++)
+	for(int i = 0; i < 10-1; i ++)
 	{
-		for(int j = i + 1; j < 10; j ++)
+		for(int j = 0; j < 10-i-1; j ++)
 		{
-			if(nums[i] > nums[j])
+			if(nums[j] > nums[j+1])
 			{
-				temp = nums[i];
-				nums[i] = nums[j];
-				nums[j] = temp;
+				temp = nums[j];
+				nums[j] = nums[j+1];
+				nums[j+1] = temp;
 			}
 		}	
 
