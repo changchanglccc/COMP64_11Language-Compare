41213553e2628f7ccfc442b67c30583b5cdcb374
Jennifer Chong Li <jennifer06091992@gmail.com>
2017-07-17 22:48:33 -0400

    Update bubble.pl

--- bubble.pl
+++ bubble.pl
@@ -4,6 +4,6 @@
 * bubble([10,99,8,2,6,5,4,3,2,1],Sorted).
 */
 bubble(Input,Sorted) :- swap(Input,List) , ! , bubble(List,Sorted).
-                        bubble(Sorted,Sorted).
+bubble(Sorted,Sorted).
 swap([A,B|List],[B,A|List]) :- A > B.
 swap([C|List],[C|List1]) :- swap(List,List1).
