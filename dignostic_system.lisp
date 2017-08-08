f2ba0945a732af0af35dd3123a90059d2dc5866c
punittrivedi <punithtrivedi@gmail.com>
2017-08-07 21:16:37 -0400

    Update dignostic_system.lisp
    Matched Functionality added

--- dignostic_system.lisp
+++ dignostic_system.lisp
@@ -1,21 +1,38 @@
-(defun diagnostic_system(symptoms disease)
+(defun diagnostic_system(symptoms aids influenza hepatitisC pregnancy)
+
 (if (find (car symptoms) '(list fever cough soreThroat tiredness headaches))
-    (if(not(find 'influenza disease))(push 'influenza disease)))
+    (setf (disease-matched influenza) (+ (disease-matched influenza) 1)))  
     
 (if (find (car symptoms) '(list muscleAches chills rash tiredness soreThroat))
-    (if(not(find 'aids disease))(push 'aids disease)))
+    (setf (disease-matched aids) (+ (disease-matched aids) 1)))
     
 (if (find (car symptoms) '(list joint_pain stomach_pain nausea dark_urine itchy_skin))
-    (if(not(find 'hepatitisC disease))(push 'hepatitisC disease)))
+    (setf (disease-matched hepatitisC) (+ (disease-matched hepatitisC) 1)))
     
 (if (find (car symptoms) '(list mood_swings food_aversions abdominal_bloating a_missed_period sore breasts))   
-    (if(not(find 'pregnancy disease))(push 'pregnancy disease)))
+    (setf (disease-matched pregnancy) (+ (disease-matched pregnancy) 1)))
    
 (if (not(null symptoms))    
-(diagnostic_system (cdr symptoms) disease)   
-(print disease)))
+(diagnostic_system (cdr symptoms) aids influenza hepatitisC pregnancy)   
+))
 
-(setq symptoms '(mood_swings food_aversions abdominal_bloating a_missed_period sore breasts))
-(setq disease '())
+
+(defstruct disease name matched) 
+(setq aids 
+      (make-disease :name "Aids" :matched 0)) 
+(setq influenza 
+      (make-disease :name "Influenza" :matched 0)) 
+(setq hepatitisC 
+      (make-disease :name "HepatitisC" :matched 0)) 
+(setq pregnancy 
+      (make-disease :name "Pregnancy" :matched 0)) 
+
+
+(setq symptoms '(fever mood_swings soreThroat joint_pain food_aversions))
 (print "Dignose Result:")
-(diagnostic_system symptoms '())
+(diagnostic_system symptoms aids influenza hepatitisC pregnancy)
+
+(print influenza)
+(print aids)
+(print hepatitisC)
+(print pregnancy)
